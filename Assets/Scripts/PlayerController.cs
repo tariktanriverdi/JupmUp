247ca1d09a3a5182c4379ee2d945640e9d36e4bd
tark_lfs@hotmail.com <tark_lfs@hotmail.com>
2021-10-30 17:54:59 +0300

    Fix Background Repeat

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,30 +5,42 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
-    public float jumpForce=10;
-    public float gravityModifier=1;
-    public bool isOnGround=true;
+    public float jumpForce = 10;
+    public float gravityModifier = 1;
+    public bool isOnGround = true;
+    private bool isGameOver = false;
     void Start()
     {
-        playerRb=GetComponent<Rigidbody>();
-        Physics.gravity*=gravityModifier;
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
     }
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space)&& isOnGround){
-          JumpUp();
-          isOnGround=false;
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            JumpUp();
+            isOnGround = false;
         }
     }
-    public void JumpUp(){
-         playerRb.AddForce(Vector3.up*jumpForce,ForceMode.Impulse);
+    public void JumpUp()
+    {
+        playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
     }
-    private void OnCollisionEnter(Collision other) {
-    
-     
-      isOnGround=true;
-        
+    private void OnCollisionEnter(Collision other)
+    {
+
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+
+        }
+        if (other.gameObject.CompareTag("Obstacle"))
+        {  
+          Debug.Log("Game Over");
+            isGameOver = true;
+        }
+
     }
 
 }
