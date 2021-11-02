f65f58c7d47d8ea2d34616c934f9c69ca860624e
tark_lfs@hotmail.com <tark_lfs@hotmail.com>
2021-11-02 20:35:12 +0300

    Player Animations Added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
+    private Animator playerAnim;
     public float jumpForce = 10;
     public float gravityModifier = 1;
     public bool isOnGround = true;
@@ -12,16 +13,21 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround&& !isGameOver)
         {
             JumpUp();
             isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
+           // playerAnim.SetFloat("Speed_f", 0);
         }
+       
+
     }
     public void JumpUp()
     {
@@ -36,8 +42,10 @@ public class PlayerController : MonoBehaviour
 
         }
         if (other.gameObject.CompareTag("Obstacle"))
-        {  
-          Debug.Log("Game Over");
+        {   
+            playerAnim.SetBool("Death_b",true);
+            playerAnim.SetInteger("DeathType_int",1);
+            Debug.Log("Game Over");
             isGameOver = true;
         }
 
